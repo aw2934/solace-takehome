817a5f94dc7c92b648a10d2bee1517284bab543e
Alex Wolinsky <alexwolinsky1@gmail.com>
2023-06-28 18:05:53 -0700

    add order by to get request

--- backend/getNotes.ts
+++ backend/getNotes.ts
@@ -5,6 +5,7 @@ const getNotes = (_req: Request, res: Response) => {
   client.query(`
     SELECT * FROM notes
     WHERE deleted = false
+    ORDER BY id ASC
   `)
     .then(n => res.status(200).send(n.rows))
     .catch(_ => res.status(500).send('Error'));
